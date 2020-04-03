1152f2ac3b07ce6323b635294d40bb4a813ad4b0
thuypham98 <off.21198@gmail.com>
2020-04-04 00:14:49 +0700

    tinh toan tu route params

--- index.js
+++ index.js
@@ -2,14 +2,47 @@ const express = require('express'); //require thư viện express
 // const app = require('express')();
 const app = express(); 
 
-// app.get('/', (request, response) => response.send('hello'));
-
 const handle = (req, res) => {
-    const name = req.params.name;
-    const age = req.params.age;
+    // const name = req.params.name;
+    // const age = req.params.age;
+
+    //or
+    const { name, age } = req.params;
     res.send(`Xin chao ${name} ${age} tuoi`);
 }
 
-app.get('/hello/:name/:age', handle); //chỉ đáp trả 1 lần
+//chỉ đáp trả 1 lần
+app.get('/hello/:name/:age', handle); 
+
+app.get('/tinh/:tenPhepTinh/:soA/:soB', (request, response) => {
+    const { tenPhepTinh, soA, soB } = request.params;
+    const pt = new Tinh(tenPhepTinh, soA, soB);
+    response.send(pt.getResultString());
+});
 
 app.listen(3000)//port 0 -> 65000
+
+class Tinh{
+    constructor(tenPhepTinh, soA, soB){
+        this.tenPhepTinh = tenPhepTinh;
+        this.soA = soA;
+        this.soB = soB;
+    }
+
+    _getChuoiPhepTinh(){    // thêm " _ " để đánh dấu - hiểu là private method
+        const { tenPhepTinh, soA, soB } = this;
+        if(tenPhepTinh === 'cong') return `${soA} + ${soB}`;
+        if(tenPhepTinh === 'tru') return `${soA} - ${soB}`;
+        if(tenPhepTinh === 'nhan') return `${soA} * ${soB}`;
+        return `${soA} / ${soB}`;
+    }
+
+    getResultString(){
+        const chuoiPhepTinh = this._getChuoiPhepTinh();
+        const result = eval(chuoiPhepTinh);
+        return `${chuoiPhepTinh} = ${result}`;
+    }
+}
+
+// const pt = new Tinh('cong', 8, 7);
+// console.log(pt.getResultString()); // 8 + 7 = 15
