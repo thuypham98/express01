49b9d44f25ac0312901c12226b4ee24359bb6e35
thuypham98 <off.21198@gmail.com>
2020-04-20 22:53:11 +0700

    crud admin

---
+++ uploadTest/index.js
@@ -0,0 +1,81 @@
+const mongoose = require('mongoose');
+const express = require('express');
+const fs = require('fs');
+
+const Product = require('./Product');
+const upload = require('./uploadConfig');
+
+mongoose.Promise = global.Promise;
+const uri = 'mongodb://localhost/shop';
+
+// mongoose.connect(uri, { useMongoClient: true });
+mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connection.once('open', () => {
+    app.listen(3000, () => console.log('Server port 3000 uploadTest started!'));
+});
+
+
+const app = express();
+app.set('view engine', 'ejs');
+app.set('views', './views');
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+    Product.find({})
+    .then(products => res.render('index', { products }));
+});
+
+app.get('/admin', (req, res) => {
+    Product.find({})
+    .then(products => res.render('admin', { products }));
+});
+
+app.get('/add', (req, res) => res.render('add')); //create screen
+
+//them san pham
+app.post('/add', upload.single('image'), (req, res) => { // image: tên của <input type="file" name="profile" />
+    const { name, video, desc } = req.body;
+    const image = req.file ? req.file.filename : 'default.jpg';
+    const product = new Product({ name, video, desc, image });
+    product.save()
+    .then(() => res.redirect('/'))
+    .catch(err => res.send(err.message));
+});
+
+//delete san pham
+app.get('/delete/:id', (req, res) => {
+    const { id } = req.params;
+    Product.findByIdAndRemove(id)
+    .then((product) => {
+        // console.log(product);
+        res.redirect('/admin');
+        if(product.image === 'default.jpg') return;
+        const path = './public/images/background/' + product.image;
+        fs.unlink(path, (err) => {
+            if(err) console.log(err.message);
+        });
+    })
+    .catch(err => res.send(err.message));
+});
+
+//edit product
+app.get('/edit/:id', (req, res) => { //show edit screen
+    const { id } = req.params;
+    Product.findById(id)
+    .then(product => res.render('editProduct', { product }));
+});
+app.post('/update/:id', upload.single('image'), (req, res) => { // update product
+    const { id } = req.params;
+    const { name, video, desc, oldImage } = req.body;
+    const image = req.file ? req.file.filename : oldImage;
+    Product.findByIdAndUpdate(id, {name, video, desc, image })
+    .then(() => {
+        res.redirect('/admin');
+        if(!req.file) return;
+        const path = './public/images/background/' + oldImage;
+        fs.unlink(path, (err) => {
+            if(err) console.log(err.message);
+        });
+    })
+});
+
